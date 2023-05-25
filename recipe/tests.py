83178cd7276ddd5b1f097744ffb4f6fc4edd5b20
ROCKET228 <michkahap@gmail.com>
2023-05-25 15:00:44 +0300

    Make main view and test for it

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe
 
-# Create your tests here.
+
+class MainViewTest(TestCase):
+    def setUp(self):
+        Recipe.objects.create(name='Recipe 1', ingredients='Ingredient 1')
+        Recipe.objects.create(name='Recipe 2', ingredients='Ingredient 2')
+
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)  # Ensure the response is successful
+        self.assertTemplateUsed(response, 'main.html')  # Ensure the correct template is used
+        self.assertQuerysetEqual(response.context['recipes'], ['<Recipe: Recipe 1>', '<Recipe: Recipe 2>'])
+
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from .models import Recipe
+
+
+def main(requests):
+    recipes = Recipe.objects.all()
+    return render(requests, 'main.html', context={'recipes':recipes})
+
+
 
